ede8e7de69cdbb09181f91a35ec1124c809ce351
Sandeep <sandeepkumar.sandy04@gmail.com>
2020-07-09 15:20:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.run(port=5002, threaded=False)
+    #app.run(port=5002, threaded=False)
     
     # Serve the app with gevent
     http_server = WSGIServer(('0.0.0.0', 5000), app)
